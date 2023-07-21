beabae4ecfa1f347f2780e7c9c1a946d52538526
Sourcery AI <>
2023-07-21 10:24:20 +0000

    'Refactored by Sourcery'

--- flexirobo/app.py
+++ flexirobo/app.py
@@ -12,7 +12,7 @@ app = Flask(__name__)
 def roll_dice(num, sides):
     # 创建一个列表来存储每次掷骰的点数
     rolls = []
-    for i in range(int(num)):
+    for _ in range(int(num)):
         # 在 1 和骰子面数之间选择一个随机整数
         roll = random.randint(1, int(sides))
         # 将随机整数添加到列表中
@@ -26,13 +26,10 @@ def roll_dice(num, sides):
 
 
 def process(text):
-    # 如果用户输入中包含 my_dict 中的某个键，则返回对应的值
-    for key, value in responses.items():
-        if key in text:
-            return value
-
-    # 如果用户输入不包含任何键，则返回默认回复
-    return '抱歉，我不明白您的意思'
+    return next(
+        (value for key, value in responses.items() if key in text),
+        '抱歉，我不明白您的意思',
+    )
 
 
 @app.route('/playground/')
@@ -55,8 +52,7 @@ def reply():
     # 在 responses 变量中查找匹配该输入语句的回复
     reply = None
     for pattern, definition in responses.items():
-        match = re.match(pattern, message)
-        if match:
+        if match := re.match(pattern, message):
             if definition['type'] == 'text':
                 reply = definition['data']
             elif definition['type'] == 'call':
